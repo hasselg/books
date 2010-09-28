dec14d64c9601ec491abe942e44a55489da1b3df
Greg Hasseler <ghasseler@gmail.com>
2010-09-27 23:59:44 -0400

    Updated to list up to the last 5 books that were read. Also updated the style sheet url, which allows us to not have to pass in the media url as part of the context from the view.

--- books/library/views.py
+++ books/library/views.py
@@ -1,8 +1,12 @@
 from django.conf import settings
 from django.shortcuts import render_to_response
 
-def homepage(request):
-    context = {'MEDIA_URL': settings.MEDIA_URL}
-    return render_to_response('library/index.html', context)
+from books.library.models import Book
 
+def homepage(request):
+    # Get the last 5 read books
+    last_five_books = Book.objects.order_by('year_read').reverse()[:5]
+    context = {
+        'last_five_books': last_five_books}
+    return render_to_response('library/index.html', context)
 
