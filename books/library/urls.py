4697a0aa4b7523b575feb487865daf0e798feade
Greg Hasseler <ghasseler@gmail.com>
2010-11-11 22:52:03 -0500

    Various URL changes.

--- books/library/urls.py
+++ books/library/urls.py
@@ -1,16 +1,23 @@
 from django.conf.urls.defaults import *
-from books.library.models import Book
+from books.library.models import Author, Book
 
-info_dict = {
+books_dict = {
     'queryset': Book.objects.all(),
 }
 
+authors_dict = {
+    'queryset': Author.objects.all(),
+}
+
 urlpatterns = patterns('',
-    (r'^$', 'books.library.views.homepage'),
+    (r'^$',                 'books.library.views.homepage'),
+    (r'^create_book/$',     'books.library.views.edit_book'),
     (r'^edit_book/(\d+)/$', 'books.library.views.edit_book'),
-    (r'^create_book/$', 'books.library.views.edit_book'),
-    (r'^edit_author/(\d+)/$', 'books.library.views.edit_author'),
-    (r'^create_author/$', 'books.library.views.add_author_ajax'),
-    (r'^list/$', 'django.views.generic.list_detail.object_list', info_dict),
+    (r'^list_books/$',      'django.views.generic.list_detail.object_list', books_dict),
+
+    (r'^create_author_ajax/$',  'books.library.views.add_author_ajax'),
+    (r'^create_author/$',       'books.library.views.edit_author'),
+    (r'^edit_author/(\d+)/$',   'books.library.views.edit_author'),
+    (r'^list_authors/$',          'django.views.generic.list_detail.object_list', authors_dict),
 )
 
