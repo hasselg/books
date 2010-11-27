6bb344e9e26bf4f698798b2badf6e336f8e9cba8
Greg Hasseler <ghasseler@gmail.com>
2010-11-27 10:18:56 -0500

    Removed some unused code.

--- books.py
+++ books.py
@@ -1,11 +1,9 @@
 import os
 import sys
-import subprocess
 
 if __name__ == '__main__':
     sys.path.append(os.getcwd())
     os.environ['DJANGO_SETTINGS_MODULE'] = 'books.settings'
-    # import twisted_server as s
     import cherry_server as s
     s.main()
     
