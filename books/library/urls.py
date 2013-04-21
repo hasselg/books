ecdb5818e3e9a4591cd4c1da156c98c7f794b0fb
Greg Hasseler <ghasseler@gmail.com>
2013-04-21 10:47:21 -0400

    Updated to use Django class-based generic views instead of the old function-based generic views.

--- books/library/urls.py
+++ books/library/urls.py
@@ -1,23 +1,16 @@
 from django.conf.urls.defaults import *
 from books.library.models import Author, Book
-
-books_dict = {
-    'queryset': Book.objects.all(),
-}
-
-authors_dict = {
-    'queryset': Author.objects.all(),
-}
+from django.views.generic.list import ListView
 
 urlpatterns = patterns('',
     (r'^$',                 'books.library.views.homepage'),
     (r'^create_book/$',     'books.library.views.edit_book'),
     (r'^edit_book/(\d+)/$', 'books.library.views.edit_book'),
-    (r'^list_books/$',      'django.views.generic.list_detail.object_list', books_dict),
+    (r'^list_books/$',      ListView.as_view(queryset=Book.objects.all())),
 
     (r'^create_author_ajax/$',  'books.library.views.add_author_ajax'),
     (r'^create_author/$',       'books.library.views.edit_author'),
     (r'^edit_author/(\d+)/$',   'books.library.views.edit_author'),
-    (r'^list_authors/$',        'django.views.generic.list_detail.object_list', authors_dict),
+    (r'^list_authors/$',        ListView.as_view(queryset=Author.objects.all())),
 )
 
